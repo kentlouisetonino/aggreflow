0c9ccdf3f8bbb76cc8b1a6a59a9db3b1e4f33805
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-10-21 01:06:20 +0800

    my first go server:

--- src/main.go
+++ src/main.go
@@ -1,26 +1,42 @@
 package main
 
-import "fmt"
-import "log"
-import "os"
-import "github.com/joho/godotenv"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	// Load the env file.
 	// No longer needs to use the "export PORT=value".
-  err := godotenv.Load(".env")
+	err := godotenv.Load(".env")
 
-  if err != nil {
-    fmt.Println(err)
-  }
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  PORT := os.Getenv("PORT")
+	PORT := os.Getenv("PORT")
 
 	if PORT == "" {
 		log.Fatal("PORT is not found in environment.")
 	}
 
+	router := chi.NewRouter()
 
-	fmt.Println(PORT)
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + PORT,
+	}
+
+	log.Printf("Server starting on port %v.", PORT)
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
