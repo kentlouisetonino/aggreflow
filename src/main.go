68d8ffc9bff5635bd10709e197c2bcc884127c31
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-10-21 10:25:26 +0800

    make it work

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -30,6 +31,15 @@ func main() {
 
 	// Initialize the router.
 	router := chi.NewRouter()
+  
+  router.Use(cors.Handler(cors.Options{
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"*"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300,
+  }))
 
 	// Setup the server.
 	server := &http.Server{
