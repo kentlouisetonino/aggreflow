f03b4df25d87b206b65fd6248acba5b80ad19d6f
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-11-12 09:41:56 +0800

    modify project

--- src/helper/handle_error.go
+++ src/helper/handle_error.go
@@ -3,5 +3,5 @@ package helper
 import "net/http"
 
 func HandleError(w http.ResponseWriter, r *http.Request) {
-  responWithError(w, 400, "Something is wrong. Please try again.")
+	responWithError(w, 400, "Something is wrong. Please try again.")
 }
--- src/helper/handle_health.go
+++ src/helper/handle_health.go
@@ -4,6 +4,5 @@ import "net/http"
 
 // This wil intercept if the api is alive and running.
 func HandleHealth(w http.ResponseWriter, r *http.Request) {
-  respondWithJSON(w, 200, struct{}{})
+	respondWithJSON(w, 200, struct{}{})
 }
-
